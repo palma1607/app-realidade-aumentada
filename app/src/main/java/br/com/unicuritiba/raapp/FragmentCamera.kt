1319b9d4d53c851ceb4dec8e143eeae7f419b861
Diego Palma <diego.palma@rentcars.com>
2024-05-27 19:16:22 -0300

    update aula 20/05

--- app/src/main/java/br/com/unicuritiba/raapp/FragmentCamera.kt
+++ app/src/main/java/br/com/unicuritiba/raapp/FragmentCamera.kt
@@ -1,24 +1,29 @@
 package br.com.unicuritiba.raapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.util.Range
 import android.view.LayoutInflater
+import android.view.Surface
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import java.util.concurrent.Executors
 
 class FragmentCamera : Fragment() {
 
     private var previewView : PreviewView? = null
-    private var camera : Camera? = null
-    private val cameraExecutor = Executors.newSingleThreadExecutor()
+    private var imageView : ImageView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +41,8 @@ class FragmentCamera : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         previewView = view.findViewById(R.id.preview_camera)
+        imageView = view.findViewById(R.id.image_view_element)
+
         previewView?.post {
             configureCamera()
         }
@@ -61,6 +68,54 @@ class FragmentCamera : Fragment() {
             .requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
 
         preview.setSurfaceProvider(previewView?.surfaceProvider)
-        cameraProvider.bindToLifecycle(viewLifecycleOwner,cameraSelector,preview)
+
+        val imageAnalysis = getImageAnalysis()
+
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(requireContext())){ image ->
+            analyzerImage(image)
+        }
+        cameraProvider.bindToLifecycle(
+            viewLifecycleOwner,
+            cameraSelector,
+            preview,
+            imageAnalysis)
+    }
+
+    private fun analyzerImage(image: ImageProxy){
+        val bitmap = image.toBitmap()
+        val width = image.width - 1
+        val height = image.height - 1
+
+        var currentRed = 0
+        var currentX = 0
+        var currentY = 0
+
+        for(x in 0..width){
+            for(y in 0..height){
+                val pixel = bitmap.getPixel(x,y)
+                val pixelColorRed = Color.red(pixel)
+                if(pixelColorRed > currentRed){
+                    currentRed = pixelColorRed
+                    currentX = x
+                    currentY = y
+                }
+            }
+        }
+        setMarkerOnPosition(currentX,currentY,currentRed)
+    }
+
+    private fun setMarkerOnPosition(positionX: Int,
+                                    positionY: Int,
+                                    intensity: Int) {
+        val layoutParams = imageView?.layoutParams as ConstraintLayout.LayoutParams
+        layoutParams.marginStart = positionX
+        layoutParams.topMargin = positionY
+        imageView?.layoutParams = layoutParams
+    }
+
+    private fun getImageAnalysis() : ImageAnalysis{
+        return ImageAnalysis.Builder()
+            .setTargetRotation(Surface.ROTATION_90)
+            .build()
     }
 }
